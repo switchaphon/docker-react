6c63c12acd982086747670161e62b8a5c4640bbd
SWP <switchaphon@SWPs-MacBook-Air.local>
2020-08-18 18:42:14 +0700

    Change app->name in .travis.yml

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Using "name" in .travis.yml
+          Using "app:" in .travis.yml
         </p>
         <a
           className="App-link"
